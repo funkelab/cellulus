6b4c901613c1bbea193ba569d8dc6686f5f04b23
lmanan <manan.lalit@gmail.com>
2023-06-04 15:24:30 -0400

    Add `visualize` method to `utils.py`

--- cellulus/utils/utils.py
+++ cellulus/utils/utils.py
@@ -4,7 +4,9 @@ import urllib
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
-
+import zarr
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
             
 def normalize_min_max_percentile(x, pmin=3, pmax=99.8, axis=None, clip=False, eps=1e-20, dtype=np.float32):
     """
@@ -39,7 +41,7 @@ def normalize_mi_ma(x, mi, ma, clip=False, eps=1e-20, dtype=np.float32):
     return x
 
 
-def extract_data(zarr_url, data_dir):
+def extract_data(zarr_url, data_dir, project_name):
     """
         Extracts data from `zip_url` to the location identified by `data_dir` parameters.
 
@@ -53,12 +55,63 @@ def extract_data(zarr_url, data_dir):
         -------
 
     """
-    if not os.path.exists(data_dir):
+    if not os.path.exists(os.path.join(data_dir, project_name)):
         os.makedirs(data_dir)
         print("Created new directory {}".format(data_dir))
     
-    with urlopen(zarr_url) as zipresp:
-        with ZipFile(BytesIO(zipresp.read())) as zfile:
-            zfile.extractall(data_dir)
-    print("Unzipped and downloaded data to the location {}".format(data_dir))
+        with urlopen(zarr_url) as zipresp:
+            with ZipFile(BytesIO(zipresp.read())) as zfile:
+                zfile.extractall(data_dir)
+        print("Downloaded and unzipped data to the location {}".format(data_dir))
+    else:
+        print("Directory already exists at the location {}".format(os.path.join(data_dir, project_name)))
+              
+def visualize(data_dir, train_val_dir='val', n_images=5, new_cmp='magma'):
+    """
+        Parameters
+        -------
+        data_dir: str
+            Place where crops are (for example: data_dir='./')
+        train_val_dir: str
+            One of 'train' or 'val'
+        n_images: int
+            Number of columns
+        new_cmp: Color Map
+
+        Returns
+        -------
+
+    """
+    font = {'family': 'serif',
+            'color': 'black',
+            'weight': 'bold',
+            'size': 16,
+            }
+    zarr_input = zarr.open(os.path.join(data_dir, train_val_dir + '.zarr'))
+    indices = np.random.randint(0, zarr_input['raw'].shape[0], n_images)
+    fig = plt.figure(constrained_layout=False, figsize=(16, 10))
+    spec = gridspec.GridSpec(ncols=n_images, nrows=3, figure=fig)
+    
+    for i, index in enumerate(indices):
+        im0 = zarr_input['raw'][index][...,0] 
+        im1 = zarr_input['raw'][index][...,1]
+        vmin0, vmax0 = np.min(im0), np.max(im0)
+        vmin1, vmax1 = np.min(im1), np.max(im1)
+        # TODO --> here the `visualize` is very specific to the tissuenet dataset
+        ax0 = fig.add_subplot(spec[0, i])
+        ax0.imshow(im0, cmap='magma', interpolation='None', vmin = vmin0, vmax = vmax0)
+        ax0.axes.get_xaxis().set_visible(False)
+        ax0.set_yticklabels([])
+        ax0.set_yticks([])
+        if i == 0:
+            ax0.set_ylabel('IM-0', fontdict=font)
+        ax1 = fig.add_subplot(spec[1, i])
+        ax1.imshow(im1, cmap='magma', interpolation='None', vmin=vmin1, vmax = vmax1)
+        ax1.axes.get_xaxis().set_visible(False)
+        ax1.set_yticklabels([])
+        ax1.set_yticks([])
+        if i == 0:
+            ax1.set_ylabel('IM-1', fontdict=font)
+    plt.tight_layout(pad=0, h_pad=0)
+    plt.show()
     
