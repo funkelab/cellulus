9a58a4fd5bb751e4bbf2d940562e9167c8c1b693
lmanan <manan.lalit@gmail.com>
2023-06-04 23:28:13 -0400

    Update `datasets` directory

--- cellulus/datasets/TwoDimensionalDataset.py
+++ cellulus/datasets/TwoDimensionalDataset.py
@@ -1,10 +1,9 @@
 import numpy as np
 import os
 import random
-import tifffile
 from cellulus.utils.utils import normalize_min_max_percentile
 from glob import glob
-
+import zarr
 
 class TwoDimensionalDataset(Dataset):
     """
@@ -15,9 +14,6 @@ class TwoDimensionalDataset(Dataset):
     image_list: list
         list of strings containing paths to the images
 
-    size: int
-        This is set equal to a different value than `real_size`, in case the epoch is designed to not be equal
-        to all the data available
     real_size: int
         Actual number of images (crops) available.
 
@@ -31,42 +27,37 @@ class TwoDimensionalDataset(Dataset):
     ----------
     __init__: Initializes an object of class `TwoDimensionalDataset`
 
-    __len__: Returns `self.real_size` if `self.size` is None
+    __len__: Returns `self.real_size`
 
     __getitem__: Returns `sample` (dictionary) which has `image` and `im_name` as keys
     """
 
-    def __init__(self, data_dir, transform=None, norm='min-max-percentile'):
+    def __init__(self, data_dir, type='train', norm='min-max-percentile', crop_size = 252, transform=None):
         print('2D data loader created! Accessing data from {}/'.format(data_dir))
 
         # get image list
-        image_list = sorted(glob(os.path.join(data_dir, 'images', '*.tif')))
-        print('Number of images in the `{}` directory is {}'.format(data_dir, len(image_list)))
-        self.image_list = image_list
-
-        self.size = size
-        self.real_size = len(self.image_list)
+        raw = zarr.open(os.path.join(data_dir, type+'.zarr'))['raw']
+        print('Number of images in the `{}` directory is {}'.format(data_dir, raw.shape[0]))
+        self.type= type
+        self.real_size = raw.shape[0]
         self.transform = transform
         self.norm = norm
         self.crop_size = crop_size
 
     def __len__(self):
-        return self.real_size if self.size is None else self.size
+        return self.real_size
 
     def __getitem__(self, index):
-        index = index if self.size is None else random.randint(0, self.real_size - 1)
+        index = random.randint(0, self.real_size - 1)
         sample = {}
 
-        # load image (currently configured for `tiff` images)
-
-        image = tifffile.imread(self.image_list[index])  # YX
-
-        # add an additional channel
-        image = image[np.newaxis, ...]  # CYX , where C = 1
+        # load image
+        image = raw[index]  # YXC
         if self.norm == 'min-max-percentile':
-            image = normalize_min_max_percentile(image, 1, 99.8, axis=(0, 1))
-        sample['image'] = image
-        sample['im_name'] = self.image_list[index]
+            image = normalize_min_max_percentile(image, 1, 99.8, axis=(0, 1)) # TODO (is this correctly done?)
+        # add an additional channel
+        image = np.transpose(image, (2, 0, 1))  # CYX , where C = 1
+        sample['image'] = image[:, self.crop_size, :self.crop_size] # TODO (cropping is not random)
 
         # transform
         if self.transform is not None:
--- cellulus/datasets/__init__.py
+++ cellulus/datasets/__init__.py
@@ -1,6 +1,6 @@
 from cellulus.datasets.TwoDimensionalDataset import TwoDimensionalDataset
-def dataset(name, dataset_opts):
-    if name =="2d":
+def get_dataset(name, dataset_opts):
+    if name =="2D":
         return TwoDimensionalDataset(**dataset_opts)
     else:
         raise RuntimeError("Dataset {} not available".format(name))
