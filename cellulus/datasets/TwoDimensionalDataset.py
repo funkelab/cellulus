97171353fd9cb4dd5f0bc990b9809f07e2dc3d34
lmanan <manan.lalit@gmail.com>
2023-06-06 09:51:06 -0400

    Correct defining `image` variable

--- cellulus/datasets/TwoDimensionalDataset.py
+++ cellulus/datasets/TwoDimensionalDataset.py
@@ -1,10 +1,13 @@
 import numpy as np
 import os
 import random
-from cellulus.utils.utils import normalize_min_max_percentile
-from glob import glob
 import zarr
+from glob import glob
+from imgaug import augmenters as iaa
 from torch.utils.data import Dataset
+
+from cellulus.utils.utils import normalize_min_max_percentile
+
 
 class TwoDimensionalDataset(Dataset):
     """
@@ -37,10 +40,10 @@ class TwoDimensionalDataset(Dataset):
         print('2D data loader created! Accessing data from {}/'.format(data_dir+'/'+type+'.zarr'))
 
         # get image list
-        raw = zarr.open(os.path.join(data_dir, type+'.zarr'))['raw']
-        print('Number of images in the `{}` directory is {}'.format(data_dir+'/'+type+'.zarr', raw.shape[0]))
+        self.raw = zarr.open(os.path.join(data_dir, type+'.zarr'))['raw']
+        print('Number of images in the `{}` directory is {}'.format(data_dir+'/'+type+'.zarr', self.raw.shape[0]))
         self.type= type
-        self.real_size = raw.shape[0]
+        self.real_size = self.raw.shape[0]
         self.transform = transform
         self.norm = norm
         self.crop_size = crop_size
@@ -53,12 +56,12 @@ class TwoDimensionalDataset(Dataset):
         sample = {}
 
         # load image
-        image = raw[index]  # YXC
+        image = self.raw[index]  # YXC
         if self.norm == 'min-max-percentile':
             image = normalize_min_max_percentile(image, 1, 99.8, axis=(0, 1)) # TODO (is this correctly done?)
         # add an additional channel
-        image = np.transpose(image, (2, 0, 1))  # CYX , where C = 1
-        sample['image'] = image[:, self.crop_size, :self.crop_size] # TODO (cropping is not random)
+        image = np.transpose(image, (2, 0, 1))  # CYX , where C = 2
+        sample['image'] = image[:, :self.crop_size, :self.crop_size] # TODO (cropping is not randomly done at the moment)
 
         # transform
         if self.transform is not None:
