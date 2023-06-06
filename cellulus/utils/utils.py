021394db8a423ffc2fed94edbb3e4232de5b9e58
lmanan <manan.lalit@gmail.com>
2023-06-06 09:51:53 -0400

    Add `Logger` and `AverageMeter` to `utils.py`

--- cellulus/utils/utils.py
+++ cellulus/utils/utils.py
@@ -1,13 +1,83 @@
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import urllib
+import zarr
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
-import zarr
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-            
+import threading
+
+
+class AverageMeter(object):
+
+    def __init__(self, num_classes=1):
+        self.num_classes = num_classes
+        self.reset()
+        self.lock = threading.Lock()
+
+    def reset(self):
+        self.sum = [0] * self.num_classes
+        self.count = [0] * self.num_classes
+        self.avg_per_class = [0] * self.num_classes
+        self.avg = 0
+
+    def update(self, val, cl=0):
+        with self.lock:
+            self.sum[cl] += val
+            self.count[cl] += 1
+            self.avg_per_class = [
+                x / y if x > 0 else 0 for x, y in zip(self.sum, self.count)]
+            self.avg = sum(self.avg_per_class) / len(self.avg_per_class)
+
+
+
+class Logger:
+
+    def __init__(self, keys, title=""):
+
+        self.data = {k: [] for k in keys}
+        self.title = title
+        self.win = None
+
+        print('Created logger with keys:  {}'.format(keys))
+
+    def plot(self, save=False, save_dir=""):
+
+        if self.win is None:
+            self.win = plt.subplots()
+        fig, ax = self.win
+        ax.cla()
+
+        keys = []
+        count = 0
+        for key in self.data:
+            if (count < 3):
+                keys.append(key)
+                data = self.data[key]
+                ax.plot(range(len(data)), data, marker='.')
+                count += 1
+        ax.legend(keys, loc='upper right')
+        ax.set_title(self.title)
+
+        plt.draw()
+        plt.close(fig)
+        Visualizer.mypause(0.001)
+
+        if save:
+            # save figure
+            fig.savefig(os.path.join(save_dir, self.title + '.png'))
+
+            # save data as csv
+            df = pd.DataFrame.from_dict(self.data)
+            df.to_csv(os.path.join(save_dir, self.title + '.csv'))
+
+    def add(self, key, value):
+        assert key in self.data, "Key not in data"
+        self.data[key].append(value)
+
+
 def normalize_min_max_percentile(x, pmin=3, pmax=99.8, axis=None, clip=False, eps=1e-20, dtype=np.float32):
     """
         Percentile-based image normalization.
