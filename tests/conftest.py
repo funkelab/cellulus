a3330785a4a8a91e896873b7a3ea4aa73e07b71a
Jan Funke <funkej@janelia.hhmi.org>
2023-06-07 17:13:18 -0400

    Update API test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+import os
+
 import pytest
+import tomli
+from cellulus.configs import ExperimentConfig
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 @pytest.fixture(scope="module")
-def training_config():
-    """Create a dummy training config for the tests."""
+def experiment_config():
+    """Create a dummy experiment config for the tests."""
 
-    # TODO
-    return None
+    with open(os.path.join(this_dir, "train.toml"), "rb") as f:
+        config = tomli.load(f)
+
+    return ExperimentConfig(**config)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,8 @@
 import cellulus
 
 
-def test_train_api(training_config) -> None:
+def test_train_api(experiment_config) -> None:
+    # limit number of iterations for this test
+    experiment_config.train_config.max_iterations = 1
 
-    # cellulus.train(training_config, max_iterations=1)
-    pass
+    cellulus.train(experiment_config)
