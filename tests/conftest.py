5e4384df50a4c13ce836abcf976dc2e069524622
Jan Funke <funkej@janelia.hhmi.org>
2023-06-07 10:12:06 -0400

    Add test scaffolding

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def training_config():
+    """Create a dummy training config for the tests."""
+
+    # TODO
+    return None
---
+++ tests/test_api.py
@@ -0,0 +1,7 @@
+import cellulus
+
+
+def test_train_api(training_config) -> None:
+
+    # cellulus.train(training_config, max_iterations=1)
+    pass
