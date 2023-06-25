18a5f9bf1cca35f6d45c71269147adfd578a6116
lmanan <manan.lalit@gmail.com>
2023-06-25 15:10:55 -0400

    Add `damping_factor`, `save_model_every` and `save_snapshot_every` parameters to `TrainConfig`

--- cellulus/configs/train_config.py
+++ cellulus/configs/train_config.py
@@ -17,15 +17,27 @@ class TrainConfig:
 
         max_iterations:
 
-            The maximim number of iterations to train for.
+            The maximum number of iterations to train for.
 
         initial_learning_rate:
 
             Initial learning rate of the optimizer.
 
-        regularizer_weight:
+        damping_factor (default = 10):
+
+            Factor used to scale the gaussian function and control the rate of damping.
+
+        regularizer_weight (default = 1e-5):
 
             The weight of the L2 regularizer on the object-centric embeddings.
+
+        save_model_every:
+
+            The model weights are saved every few iterations.
+
+        save_snapshot_every:
+
+            The zarr snapshot is saved every few iterations.
 
         train_data_config:
 
@@ -46,4 +58,7 @@ class TrainConfig:
     initial_learning_rate: float = attrs.field(
         default=4e-5, validator=instance_of(float)
     )
+    damping_factor: float = attrs.field(default=10.0, validator=instance_of(float))
     regularizer_weight: float = attrs.field(default=1e-5, validator=instance_of(float))
+    save_model_every: int = attrs.field(default=1_000, validator=instance_of(int))
+    save_snapshot_every: int = attrs.field(default=1_000, validator=instance_of(int))
