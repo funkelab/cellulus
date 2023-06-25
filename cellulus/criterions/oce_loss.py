80a765f919f1670c6f67c336276355d0192cbb4d
lmanan <manan.lalit@gmail.com>
2023-06-25 19:30:22 -0400

    Add loss calculation

---
+++ cellulus/criterions/oce_loss.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+
+class OCELoss(nn.Module): # type: ignore
+    def __init__(self, temperature: float, regularization_weight: float): 
+        super().__init__()
+        self.temperature = temperature
+        self.regularization_weight = regularization_weight
+        print("=" * 20)
+        print(
+            "Created OCE Loss-object with temperature={} and regularization={}".format(
+                temperature, regularization_weight
+            )
+        )
+        print("=" * 20)
+
+    def distance_function(self, e0, e1):
+        diff = e0 - e1
+        return diff.norm(2, dim=-1)
+
+    def nonlinearity(self, distance):
+        return 1 - (-distance.pow(2) / self.temperature).exp()
+
+    def forward(self, anchor_embedding, reference_embedding):
+        dist = self.distance_function(anchor_embedding, reference_embedding.detach())
+        nonlinear_dist = self.nonlinearity(dist)
+        return nonlinear_dist.sum()
