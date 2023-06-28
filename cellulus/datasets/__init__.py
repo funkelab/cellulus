e2f8ce3631d69f4b91f00b7ef203ea76e638bfa6
lmanan <manan.lalit@gmail.com>
2023-06-28 17:07:32 -0400

    Use control point jitter as parameter to get_dataset

--- cellulus/datasets/__init__.py
+++ cellulus/datasets/__init__.py
@@ -1,17 +1,17 @@
 from typing import Tuple
 
-from cellulus.configs.dataset_config import DatasetConfig
+from cellulus.configs import DatasetConfig
 from cellulus.datasets.zarr_dataset import ZarrDataset
 
 
 def get_dataset(
-    path: Path,
+    dataset_config: DatasetConfig,
     crop_size: Tuple[int],
     control_point_spacing: int,
-    control_point_spacing: float,
+    control_point_jitter: float,
 ) -> ZarrDataset:
     return ZarrDataset(
-        path=path,
+        dataset_config=dataset_config,
         crop_size=crop_size,
         control_point_spacing=control_point_spacing,
         control_point_jitter=control_point_jitter,
