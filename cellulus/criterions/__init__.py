8fbb2425c084a66a893f68fb0ac0e84cd6375fb3
lmanan <manan.lalit@gmail.com>
2023-06-27 21:22:57 -0400

    Add density and kappa as parameters

--- cellulus/criterions/__init__.py
+++ cellulus/criterions/__init__.py
@@ -1,5 +1,5 @@
 from cellulus.criterions.oce_loss import OCELoss
 
 
-def get_loss(temperature, regularizer_weight):
-    return OCELoss(temperature, regularizer_weight)
+def get_loss(temperature, regularizer_weight, density, kappa):
+    return OCELoss(temperature, regularizer_weight, density, kappa)
