3c1701fa60aa3c9cd79b953bb5e639a115d43859
h-westmacott <h.j.westmacott@gmail.com>
2023-07-03 14:22:21 +0100

    Add path to state_dict to inference_config

--- cellulus/configs/inference_config.py
+++ cellulus/configs/inference_config.py
@@ -2,6 +2,8 @@ import attrs
 
 from .dataset_config import DatasetConfig
 from .model_config import ModelConfig
+from attrs.validators import instance_of
+
 
 
 @attrs.define
@@ -12,12 +14,17 @@ class InferenceConfig:
 
         The model configuration.
 
+    state_dict_path:
+
+        The path to the torch state_dict for the trained model used for inference.
+
     inference_data_config:
 
         Configuration object for the data to predict and segment.
     """
 
     model_config: ModelConfig = attrs.field(converter=lambda d: ModelConfig(**d))
+    state_dict_path: str = attrs.field(validator=instance_of(str))
     inference_data_config: DatasetConfig = attrs.field(
         converter=lambda d: DatasetConfig(**d)
     )
