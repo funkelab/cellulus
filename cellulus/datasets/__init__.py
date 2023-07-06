aec5333fb5ef3ea872399d2fa16543a33be03cf4
lmanan <manan.lalit@gmail.com>
2023-07-06 10:19:57 -0400

    Add an if-else for choosing ZarrDataset or TestZarrDataset

--- cellulus/datasets/__init__.py
+++ cellulus/datasets/__init__.py
@@ -1,18 +1,25 @@
 from typing import Tuple
 
 from cellulus.configs import DatasetConfig
+from cellulus.datasets.test_zarr_dataset import TestZarrDataset
 from cellulus.datasets.zarr_dataset import ZarrDataset
 
 
 def get_dataset(
+    mode: str,
     dataset_config: DatasetConfig,
     crop_size: Tuple[int],
-    control_point_spacing: int,
-    control_point_jitter: float,
-) -> ZarrDataset:
-    return ZarrDataset(
-        dataset_config=dataset_config,
-        crop_size=crop_size,
-        control_point_spacing=control_point_spacing,
-        control_point_jitter=control_point_jitter,
-    )
+    control_point_spacing: int | None = None,
+    control_point_jitter: float | None = None,
+) -> ZarrDataset | TestZarrDataset:
+    if mode == "train":
+        return ZarrDataset(
+            dataset_config=dataset_config,
+            crop_size=crop_size,
+            control_point_spacing=control_point_spacing,
+            control_point_jitter=control_point_jitter,
+        )
+    elif mode == "eval":
+        return TestZarrDataset(dataset_config=dataset_config, crop_size=crop_size)
+    else:
+        raise RuntimeError(f"Mode {mode} not available")
