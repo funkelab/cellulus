9791c4dfe385fa618107e0beb350602598c6161e
lmanan <manan.lalit@gmail.com>
2023-07-06 10:21:49 -0400

    Reformat logger.py

--- cellulus/utils/logger.py
+++ cellulus/utils/logger.py
@@ -1,15 +1,16 @@
-import pandas as pd
-from typing import List, Dict
+from typing import Dict, List
+
 import matplotlib.pyplot as plt
+import pandas as pd
 
-class Logger: 
 
+class Logger:
     def __init__(self, keys: List[str], title: str):
         self.keys = keys
         self.title = title
         self.window = plt.subplots()
         self.data: Dict[str, List[float]] = {k: [] for k in keys}
-        print('Created logger with keys: {}'.format(keys))
+        print(f"Created logger with keys: {keys}")
 
     def add(self, key, value):
         assert key in self.data, "Key not in data"
@@ -17,18 +18,18 @@ class Logger:
 
     def write(self):
         for key in self.data:
-            data = self.data[key]
+            self.data[key]
 
         df = pd.DataFrame.from_dict(self.data)
-        df.to_csv(self.title+'.csv')
+        df.to_csv(self.title + ".csv")
 
     def plot(self):
         fig, ax = self.window
         ax.cla()
         for key in self.data:
             data = self.data[key]
-            ax.plot(range(len(data)), data, marker='.')
-        ax.set_xlabel('Iteration')
+            ax.plot(range(len(data)), data, marker=".")
+        ax.set_xlabel("Iteration")
         ax.set_ylabel(self.title)
         plt.draw()
-        plt.savefig(self.title+'.png')
+        plt.savefig(self.title + ".png")
