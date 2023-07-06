80ce217ed739c9aec842790d957040d5d8da4fae
lmanan <manan.lalit@gmail.com>
2023-07-06 10:43:54 -0400

    Remove state_dict_path since it is available within model_config.checkpoint

--- cellulus/configs/inference_config.py
+++ cellulus/configs/inference_config.py
@@ -1,30 +1,22 @@
-import attrs
+from typing import Tuple
 
-from .dataset_config import DatasetConfig
-from .model_config import ModelConfig
+import attrs
 from attrs.validators import instance_of
 
+from .dataset_config import DatasetConfig
 
 
 @attrs.define
 class InferenceConfig:
     """Inference configuration.
 
-    model_config:
-
-        The model configuration.
-
-    state_dict_path:
-
-        The path to the torch state_dict for the trained model used for inference.
-
     inference_data_config:
 
         Configuration object for the data to predict and segment.
     """
 
-    model_config: ModelConfig = attrs.field(converter=lambda d: ModelConfig(**d))
-    state_dict_path: str = attrs.field(validator=instance_of(str))
     inference_data_config: DatasetConfig = attrs.field(
         converter=lambda d: DatasetConfig(**d)
     )
+
+    crop_size: Tuple = attrs.field(default=(252, 252), validator=instance_of(Tuple))
