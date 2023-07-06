398f47f69ae09139800720fbdd93ebca80cabaf0
lmanan <manan.lalit@gmail.com>
2023-07-06 10:45:30 -0400

    Update infer.py

--- cellulus/infer.py
+++ cellulus/infer.py
@@ -1,50 +1,67 @@
+import os
+
+import torch
+
+from cellulus.datasets import get_dataset
 from cellulus.datasets.meta_data import DatasetMetaData
 from cellulus.models import get_model
-from cellulus.post_process import post_process
 from cellulus.predict import predict
-from cellulus.segment import segment
-import torch
+
+# from cellulus.segment import segment
+torch.backends.cudnn.benchmark = True
 
 
 def infer(experiment_config):
-    ...
-    # basic structure should be as follows:
-
-    # load model
-    # load dataset to predict on
-    # gunpowder scan across each file in predict dataset
-    # save out result in user-specified location
-
-    # we need a dataset for the data we're predicting on
-    # We need to make a custom dataset, that inherits from zarr_dataset, but overrides the __setup_pipeline so that we get a gunpowder scan instead of a gunpowder random_location+augment
+    print(experiment_config)
 
     inference_config = experiment_config.inference_config
-    dataset_meta_data = DatasetMetaData(inference_config.predict_data_config)
+    DatasetMetaData(inference_config.inference_data_config)
+    model_config = experiment_config.model_config
 
-    # we need the specs for the model that was used for training, right?
-    model = get_model(
-        in_channels=dataset_meta_data.get_num_channels(),
-        out_channels=dataset_meta_data.get_num_channels(),
-        num_fmaps=experiment_config.model_config.num_fmaps,
-        fmap_inc_factor=experiment_config.model_config.fmap_inc_factor,
-        features_in_last_layer=experiment_config.model_config.features_in_last_layer,
-        downsampling_factors=experiment_config.model_config.downsampling_factors,
-        num_spatial_dims=dataset_meta_data.get_num_spatial_dims(),
+    # create test_dataset
+    test_dataset = get_dataset(
+        mode="eval",
+        dataset_config=inference_config.inference_data_config,
+        crop_size=inference_config.crop_size,
     )
 
+    # create test dataloader
+    test_dataloader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=1, shuffle=False
+    )
+
+    # set model
+    model = get_model(
+        in_channels=test_dataset.get_num_channels(),
+        out_channels=test_dataset.get_num_channels(),
+        num_fmaps=model_config.num_fmaps,
+        fmap_inc_factor=model_config.fmap_inc_factor,
+        features_in_last_layer=model_config.features_in_last_layer,
+        downsampling_factors=model_config.downsampling_factors,
+        num_spatial_dims=test_dataset.get_num_spatial_dims(),
+    )
+    model = model.cuda()
+
     # load checkpoint
-    model.load_state_dict(torch.load(inference_config.state_dict_path))
+    if os.path.exists(model_config.checkpoint):
+        state = torch.load(model_config.checkpoint)
+        model.load_state_dict(state["model_state_dict"], strict=True)
+    else:
+        assert (
+            False
+        ), f"Model weights do not exist at this location :{model_config.checkpoint}!"
 
-    # load checkpoint?
+    # set in eval mode
+    model.eval()
 
-    # prediction is a dataset_config containing the location of the embedding zarr container
-    prediction = predict(inference_config, model, dataset_meta_data)
+    # create test dataloader
+    test_dataloader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=1, shuffle=False
+    )
 
-    # if segmentation is also a dataset config as suggested, does this require that segmentation and prediction are in different containers?
-    segment(prediction, inference_config)
+    # get predicted embeddings
+    predict(test_dataloader, model, inference_config)
 
-    post_process(segment, inference_config)
+    # segment(prediction, inference_config)
 
-
-def load_model():
-    pass
+    # post_process(segment, inference_config)
