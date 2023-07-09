8978991ceb419a4a2270dc3d6b7f1f0796d5ad5a
lmanan <manan.lalit@gmail.com>
2023-07-09 14:31:55 -0400

    Add noise to raw images during inference

--- cellulus/models/unet.py
+++ cellulus/models/unet.py
@@ -22,6 +22,7 @@ class UNetModel(nn.Module):  # type: ignore
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.features_in_last_layer = features_in_last_layer
+        self.mode = "train"
         self.backbone = UNet(
             in_channels=self.in_channels,
             num_fmaps=num_fmaps,
@@ -63,10 +64,34 @@ class UNetModel(nn.Module):  # type: ignore
                 nn.Conv3d(self.features_in_last_layer, out_channels, 1),
             )
 
+    def set_infer(self, p_salt_pepper, num_infer_iterations):
+        self.mode = "infer"
+        self.p_salt_pepper = p_salt_pepper
+        self.num_infer_iterations = num_infer_iterations
+
     def head_forward(self, backbone_output):
         out_head = self.head(backbone_output)
         return out_head
 
     def forward(self, raw):
-        h = self.backbone(raw)
-        return self.head_forward(h)
+        if self.mode == "train":
+            h = self.backbone(raw)
+            return self.head_forward(h)
+        elif self.mode == "infer":
+            predictions = []
+            for val in [0.5, 1.0]:
+                for _ in range(self.num_infer_iterations):
+                    noisy_input = raw.detach().clone()
+                    rnd = torch.rand(*noisy_input.shape).cuda()
+                    noisy_input[rnd <= self.p_salt_pepper] = val
+                    pred = (
+                        self.head_forward(self.backbone(noisy_input))[0].detach().cpu()
+                    )
+                    predictions.append(pred)
+
+            embedding_std, embedding_mean = torch.std_mean(
+                torch.stack(predictions, dim=0), dim=0, keepdim=False, unbiased=False
+            )
+            embedding_std = embedding_std.sum(dim=0, keepdim=True)
+            embedding = torch.cat((embedding_mean, embedding_std), dim=0)
+            return torch.unsqueeze(embedding, dim=0)
