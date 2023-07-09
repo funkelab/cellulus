ad7500655bd2bb06f544d8d5511bd3d1c16af816
lmanan <manan.lalit@gmail.com>
2023-07-09 14:29:24 -0400

    Add docstring for parameters linked to salt-pepper noise

--- cellulus/configs/inference_config.py
+++ cellulus/configs/inference_config.py
@@ -13,6 +13,23 @@ class InferenceConfig:
     dataset_config:
 
         Configuration object for the data to predict and segment.
+
+    output_dataset_config:
+
+        Configuration object produced by predict.py.
+
+    crop_size:
+
+        ROI used by the scan node in gunpowder.
+
+    p_salt_pepper:
+
+        Fraction of pixels that will have salt-pepper noise.
+
+    num_infer_iterations:
+
+        Number of times the salt-peper noise is added to the raw image.
+
     """
 
     dataset_config: DatasetConfig = attrs.field(converter=lambda d: DatasetConfig(**d))
@@ -22,3 +39,5 @@ class InferenceConfig:
     )
 
     crop_size: Tuple = attrs.field(default=(252, 252), validator=instance_of(Tuple))
+    p_salt_pepper = attrs.field(default=0.1, validator=instance_of(float))
+    num_infer_iterations = attrs.field(default=16, validator=instance_of(int))
