b0c7620cefc757a9fa853985087abf3549db1ccf
lmanan <manan.lalit@gmail.com>
2023-07-21 18:11:53 -0400

    Add reduce_mean to __init__.py

--- cellulus/criterions/__init__.py
+++ cellulus/criterions/__init__.py
@@ -1,5 +1,9 @@
 from cellulus.criterions.oce_loss import OCELoss
 
 
-def get_loss(temperature, regularizer_weight, density, kappa, num_spatial_dims):
-    return OCELoss(temperature, regularizer_weight, density, kappa, num_spatial_dims)
+def get_loss(
+    temperature, regularizer_weight, density, kappa, num_spatial_dims, reduce_mean
+):
+    return OCELoss(
+        temperature, regularizer_weight, density, kappa, num_spatial_dims, reduce_mean
+    )
