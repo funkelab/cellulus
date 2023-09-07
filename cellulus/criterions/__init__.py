79c47c810b0d272fd2508b3ca77a9f40cf243386
lmanan <manan.lalit@gmail.com>
2023-09-07 16:01:47 -0400

    Add device parameter to get_loss

--- cellulus/criterions/__init__.py
+++ cellulus/criterions/__init__.py
@@ -2,8 +2,20 @@ from cellulus.criterions.oce_loss import OCELoss
 
 
 def get_loss(
-    temperature, regularizer_weight, density, kappa, num_spatial_dims, reduce_mean
+    temperature,
+    regularizer_weight,
+    density,
+    kappa,
+    num_spatial_dims,
+    reduce_mean,
+    device,
 ):
     return OCELoss(
-        temperature, regularizer_weight, density, kappa, num_spatial_dims, reduce_mean
+        temperature,
+        regularizer_weight,
+        density,
+        kappa,
+        num_spatial_dims,
+        reduce_mean,
+        device,
     )
