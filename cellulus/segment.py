83e904ef462cf4a7ecb2761b2f4cb8a88400a7fc
lmanan <manan.lalit@gmail.com>
2023-11-07 15:57:16 -0500

    Update size of segmentation dataset

--- cellulus/segment.py
+++ cellulus/segment.py
@@ -22,7 +22,7 @@ def segment(inference_config: InferenceConfig) -> None:
         inference_config.segmentation_dataset_config.dataset_name,
         shape=(
             dataset_meta_data.num_samples,
-            1,
+            inference_config.num_bandwidth_levels,
             *dataset_meta_data.spatial_array,
         ),
         dtype=np.uint16,
@@ -40,15 +40,16 @@ def segment(inference_config: InferenceConfig) -> None:
         embeddings_mean = embeddings[
             np.newaxis, : dataset_meta_data.num_spatial_dims, ...
         ]
-        segmentation = mean_shift_segmentation(
-            embeddings_mean,
-            embeddings_std,
-            bandwidth=inference_config.bandwidth,
-            min_size=inference_config.min_size,
-            reduction_probability=inference_config.reduction_probability,
-        )
-        ds_segmentation[
-            sample,
-            0,
-            ...,
-        ] = segmentation
+        for level in range(inference_config.num_bandwidth_levels):
+            segmentation = mean_shift_segmentation(
+                embeddings_mean,
+                embeddings_std,
+                bandwidth=inference_config.bandwidth / (level + 1),
+                min_size=inference_config.min_size,
+                reduction_probability=inference_config.reduction_probability,
+            )
+            ds_segmentation[
+                sample,
+                level,
+                ...,
+            ] = segmentation
