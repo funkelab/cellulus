366a617b4dbd4a130dd22fff7c0ddf4a240aba8e
lmanan <manan.lalit@gmail.com>
2023-11-09 13:33:17 -0500

    Create a binary segmentation mask dataset

--- cellulus/segment.py
+++ cellulus/segment.py
@@ -12,8 +12,8 @@ def segment(inference_config: InferenceConfig) -> None:
     dataset_config = inference_config.dataset_config
     dataset_meta_data = DatasetMetaData.from_dataset_config(dataset_config)
 
-    f = zarr.open(inference_config.prediction_dataset_config.container_path)
-    ds = f[inference_config.prediction_dataset_config.dataset_name]
+    f = zarr.open(inference_config.segmentation_dataset_config.container_path)
+    ds = f[inference_config.segmentation_dataset_config.source_dataset_name]
 
     # prepare the zarr dataset to write to
     f_segmentation = zarr.open(
@@ -35,15 +35,36 @@ def segment(inference_config: InferenceConfig) -> None:
     ds_segmentation.attrs["resolution"] = (1,) * dataset_meta_data.num_spatial_dims
     ds_segmentation.attrs["offset"] = (0,) * dataset_meta_data.num_spatial_dims
 
+    # prepare the zarr dataset to write to
+    zarr.open(inference_config.segmentation_dataset_config.container_path)
+    ds_binary_segmentation = f_segmentation.create_dataset(
+        "binary_" + inference_config.segmentation_dataset_config.dataset_name,
+        shape=(
+            dataset_meta_data.num_samples,
+            inference_config.num_thresholds,
+            *dataset_meta_data.spatial_array,
+        ),
+        dtype=np.uint16,
+    )
+
+    ds_binary_segmentation.attrs["axis_names"] = ["s", "c"] + ["t", "z", "y", "x"][
+        -dataset_meta_data.num_spatial_dims :
+    ]
+    ds_binary_segmentation.attrs["resolution"] = (
+        1,
+    ) * dataset_meta_data.num_spatial_dims
+    ds_binary_segmentation.attrs["offset"] = (0,) * dataset_meta_data.num_spatial_dims
+
     for sample in tqdm(range(dataset_meta_data.num_samples)):
         embeddings = ds[sample]
         embeddings_std = embeddings[-1, ...]
         embeddings_mean = embeddings[
             np.newaxis, : dataset_meta_data.num_spatial_dims, ...
-        ]
-        threshold = threshold_otsu(embeddings_std)
+        ].copy()
+        thresh_otsu = threshold_otsu(embeddings_std)
         for level in range(inference_config.num_thresholds):
-            threshold += level * 0.05
+            threshold = thresh_otsu + level * 0.1
+            ds_binary_segmentation[sample, level, ...] = embeddings_std < threshold
             segmentation = mean_shift_segmentation(
                 embeddings_mean,
                 embeddings_std,
@@ -52,6 +73,12 @@ def segment(inference_config: InferenceConfig) -> None:
                 reduction_probability=inference_config.reduction_probability,
                 threshold=threshold,
             )
+            # Note that the line below is needed
+            # because the embeddings_mean is modified
+            # by mean_shift_segmentation
+            embeddings_mean = embeddings[
+                np.newaxis, : dataset_meta_data.num_spatial_dims, ...
+            ].copy()
             ds_segmentation[
                 sample,
                 level,
