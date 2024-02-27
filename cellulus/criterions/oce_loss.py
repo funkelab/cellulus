6da255eff6a0bb0a7e79c208238e67a447d983a0
lmanan <manan.lalit@gmail.com>
2024-02-26 21:33:19 -0500

    Move kappa parameter to ZarrDataset

--- cellulus/criterions/oce_loss.py
+++ cellulus/criterions/oce_loss.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 import torch.nn as nn
 
@@ -9,9 +8,7 @@ class OCELoss(nn.Module):  # type: ignore
         temperature: float,
         regularization_weight: float,
         density: float,
-        kappa: float,
         num_spatial_dims: int,
-        reduce_mean: bool,
         device: torch.device,
     ):
         """Class definition for loss.
@@ -30,16 +27,8 @@ class OCELoss(nn.Module):  # type: ignore
                 Determines the fraction of patches to sample per crop,
                 during training.
 
-            kappa:
-                Neighborhood radius to extract patches from.
-
             num_spatial_dims:
                 Should be equal to 2 for 2D and 3 for 3D.
-
-            reduce_mean:
-                Should be set to True if the loss should be averaged over all
-                pixels, and set to False, if the sum of the loss over all pixels
-                is expected.
 
             device:
                 The device to train on.
@@ -50,131 +39,25 @@ class OCELoss(nn.Module):  # type: ignore
         self.temperature = temperature
         self.regularization_weight = regularization_weight
         self.density = density
-        self.kappa = kappa
         self.num_spatial_dims = num_spatial_dims
-        self.reduce_mean = reduce_mean
         self.device = device
 
-    def distance_function(self, e0, e1):
-        diff = e0 - e1
-        return diff.norm(2, dim=-1)
+    @staticmethod
+    def distance_function(embedding_0, embedding_1):
+        difference = embedding_0 - embedding_1
+        return difference.norm(2, dim=-1)
 
-    def nonlinearity(self, distance):
+    def non_linearity(self, distance):
         return 1 - (-distance.pow(2) / self.temperature).exp()
 
-    def forward(self, prediction):
-        if self.num_spatial_dims == 2:
-            b, c, h, w = prediction.shape
-
-            h_ = h - 2 * self.kappa  # unbiased height
-            w_ = w - 2 * self.kappa  # unbiased width
-
-            num_anchors = int(self.density * h_ * w_)
-            anchor_coordinates_y = np.random.randint(
-                self.kappa, h - self.kappa, num_anchors
-            )
-            anchor_coordinates_x = np.random.randint(
-                self.kappa, w - self.kappa, num_anchors
-            )
-            anchor_coordinates = np.stack(
-                (anchor_coordinates_x, anchor_coordinates_y), axis=1
-            )  # N x 2
-        elif self.num_spatial_dims == 3:
-            b, c, d, h, w = prediction.shape
-
-            d_ = d - 2 * self.kappa  # unbiased depth
-            h_ = h - 2 * self.kappa  # unbiased height
-            w_ = w - 2 * self.kappa  # unbiased width
-
-            num_anchors = int(self.density * d_ * h_ * w_)
-            anchor_coordinates_z = np.random.randint(
-                self.kappa, d - self.kappa, num_anchors
-            )
-            anchor_coordinates_y = np.random.randint(
-                self.kappa, h - self.kappa, num_anchors
-            )
-            anchor_coordinates_x = np.random.randint(
-                self.kappa, w - self.kappa, num_anchors
-            )
-            anchor_coordinates = np.stack(
-                (anchor_coordinates_x, anchor_coordinates_y, anchor_coordinates_z),
-                axis=1,
-            )  # N x 3
-        num_references = int(self.density * np.pi * self.kappa**2)
-        anchor_coordinates = np.repeat(anchor_coordinates, num_references, axis=0)
-        offsets = self.sample_offsets(
-            radius=self.kappa,
-            num_samples=len(anchor_coordinates),
-        )
-        reference_coordinates = anchor_coordinates + offsets
-        anchor_coordinates = anchor_coordinates[np.newaxis, ...]
-        reference_coordinates = reference_coordinates[np.newaxis, ...]
-        anchor_coordinates = torch.from_numpy(np.repeat(anchor_coordinates, b, 0)).to(
-            self.device
-        )
-        reference_coordinates = torch.from_numpy(
-            np.repeat(reference_coordinates, b, 0)
-        ).to(self.device)
-        anchor_embeddings = self.get_embeddings(
-            prediction,
-            anchor_coordinates,
-        )  # B x N x 2/3
-        reference_embeddings = self.get_embeddings(
-            prediction,
-            reference_coordinates,
-        )  # B x N x 2/3
+    def forward(self, anchor_embedding, reference_embedding):
         distance = self.distance_function(
-            anchor_embeddings, reference_embeddings.detach()
+            anchor_embedding, reference_embedding.detach()
         )
-        oce_loss = self.nonlinearity(distance)
-        regularization_loss = self.regularization_weight * anchor_embeddings.norm(
-            2, dim=-1
+        non_linear_distance = self.non_linearity(distance)
+        oce_loss = non_linear_distance.sum()
+        regularization_loss = (
+            self.regularization_weight * anchor_embedding.norm(2, dim=-1).sum()
         )
-
         loss = oce_loss + regularization_loss
-        if self.reduce_mean:
-            return loss.mean(), oce_loss.mean(), regularization_loss.mean()
-        else:
-            return loss.sum(), oce_loss.sum(), regularization_loss.sum()
-
-    def sample_offsets(self, radius, num_samples):
-        if self.num_spatial_dims == 2:
-            offset_x = np.random.randint(-radius, radius + 1, size=2 * num_samples)
-            offset_y = np.random.randint(-radius, radius + 1, size=2 * num_samples)
-
-            offset_coordinates = np.stack((offset_x, offset_y), axis=1)
-        elif self.num_spatial_dims == 3:
-            offset_x = np.random.randint(-radius, radius + 1, size=3 * num_samples)
-            offset_y = np.random.randint(-radius, radius + 1, size=3 * num_samples)
-            offset_z = np.random.randint(-radius, radius + 1, size=3 * num_samples)
-
-            offset_coordinates = np.stack((offset_x, offset_y, offset_z), axis=1)
-        in_circle = (offset_coordinates**2).sum(axis=1) < radius**2
-        offset_coordinates = offset_coordinates[in_circle]
-        not_zero = np.absolute(offset_coordinates).sum(axis=1) > 0
-        offset_coordinates = offset_coordinates[not_zero]
-        if len(offset_coordinates) < num_samples:
-            return self.sample_offsets(radius, num_samples)
-
-        return offset_coordinates[:num_samples]
-
-    def get_embeddings(self, predictions, coordinates):
-        selection = []
-        for prediction, coordinate in zip(predictions, coordinates):
-            if self.num_spatial_dims == 2:
-                embedding = prediction[
-                    :, coordinate[:, 1].long(), coordinate[:, 0].long()
-                ]
-            elif self.num_spatial_dims == 3:
-                embedding = prediction[
-                    :,
-                    coordinate[:, 2].long(),
-                    coordinate[:, 1].long(),
-                    coordinate[:, 0].long(),
-                ]
-            embedding = embedding.transpose(1, 0)
-            embedding += coordinate
-            selection.append(embedding)
-
-        # selection.shape = (b, c, p) where p is the number of selected positions
-        return torch.stack(selection, dim=0)
+        return loss, oce_loss, regularization_loss
