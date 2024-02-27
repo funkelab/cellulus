26fc61d28d073d55c539af0e7718f55e8df1f88b
lmanan <manan.lalit@gmail.com>
2024-02-26 21:30:19 -0500

    Refactor __init__.py

--- cellulus/configs/__init__.py
+++ cellulus/configs/__init__.py
@@ -1,4 +1,4 @@
 from .dataset_config import DatasetConfig
 from .experiment_config import ExperimentConfig
 
-__all__ = ["ExperimentConfig", "DatasetConfig"]
+__all__ = ["DatasetConfig", "ExperimentConfig"]
