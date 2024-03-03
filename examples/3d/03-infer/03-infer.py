37ab3ced2e2bc07cdd9f416636faa12ee2506ec4
lmanan <manan.lalit@gmail.com>
2024-03-03 11:31:02 -0500

    Deployed 97d8e5c with MkDocs version: 1.5.3

--- examples/3d/03-infer/03-infer.py
+++ examples/3d/03-infer/03-infer.py
@@ -17,6 +17,7 @@ from cellulus.configs.inference_config import InferenceConfig
 from cellulus.configs.model_config import ModelConfig
 from cellulus.infer import infer
 from cellulus.utils.misc import visualize_2d
+from IPython.utils import io
 from matplotlib.colors import ListedColormap
 
 # ## Specify config values for datasets
@@ -59,7 +60,7 @@ segmentation_dataset_config = DatasetConfig(
 # should be available in the `models` directory.
 
 torch.hub.download_url_to_file(
-    url="https://github.com/funkelab/cellulus/releases/download/v0.0.1-tag/2d-demo-model.zip",
+    url="https://github.com/funkelab/cellulus/releases/download/v0.0.1-tag/3d-demo-model.zip",
     dst="pretrained_model",
     progress=True,
 )
@@ -117,8 +118,8 @@ experiment_config = ExperimentConfig(
 # Now we are ready to start the inference!! <br>
 # (To see the output of the cell below, remove the first line `io.capture_output()`).
 
-# with io.capture_output() as captured:
-infer(experiment_config)
+with io.capture_output() as captured:
+    infer(experiment_config)
 
 # ## Inspect predictions
 
